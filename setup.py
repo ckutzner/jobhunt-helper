46530092f40f6f8d5ee1fa01ea1f3b3bd248950f
Camilla Kutzner <develop@camilla-kutzner.de>
2016-04-22 15:14:27 +0200

    started work on setup program

---
+++ setup.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+""" Setup - Programm
+    Interview - nimmt Userdaten entgegen 
+    generiert die Datenbank + Tabelle
+    determiniert die wichtigen Verzeichnisse (template data, Arbeitsverzeichnis) und speichert sie für alle Programmteile, die darauf zugreifen müssen, lesbar ab -> muss was über os lernen!"""
+
