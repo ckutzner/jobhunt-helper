3cf11672780c9ac392723e03e90b3fdbfc759277
Camilla Kutzner <develop@camilla-kutzner.de>
2016-04-24 00:42:57 +0200

    sketched out init function for letter header

---
+++ init_latex.py
@@ -0,0 +1,7 @@
+"""Modules for part of the setup: generates headers for LaTeX docs """
+
+def letterheader():
+    """
+    generates header for cover letter, puts it in template directory 
+    needs to read from a text file or so where to put template
+    """
